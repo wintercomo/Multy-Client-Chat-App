58287636d3e17a59967a163f9ca2ea34ac5656d9
chris buter <chris.buter@live.nl>
2019-02-06 20:33:24 +0100

    Almost done with styling the form.
    TODO:
    + add labels to the input fields

--- MultyClientChatApp/Form1.cs
+++ MultyClientChatApp/Form1.cs
@@ -35,11 +35,35 @@ namespace MultyClientChatApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            this.Text = "testssss";
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)   
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
