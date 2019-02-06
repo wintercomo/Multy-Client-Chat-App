f80179d9c36cc51842c623110200ddcc96b40512
chris buter <chris.buter@live.nl>
2019-02-06 16:47:37 +0100

    Initial commit v2

---
+++ MultyClientChatApp/demoPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultyClientChatApp
+{
+    public class demoPerson
+    {
+        public string Naam { get; set; }
+        public string Leeftijd { get; set; }
+    }
+}
