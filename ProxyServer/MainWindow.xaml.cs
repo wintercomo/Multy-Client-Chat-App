8237911e27cd235cd747bf4a24ba792f7407d8f4
wintercomo <32674855+wintercomo@users.noreply.github.com>
2019-02-26 17:07:48 +0100

    WIP Adding a Proxy server +  Added WPF project for the proxy server + responsive design made TODO: + find out why changing 1 thing breaks the responsiveness

---
+++ ProxyServer/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProxyServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            logListBox.Items.Add("Log van:\r\n" +
+                "request headers\r\n" +
+                "Response headers\r\n" +
+                "content in\r\n" +
+                "content uit\r\n" +
+                "Client (Which browser is connected)");
+        }
+    }
+}
